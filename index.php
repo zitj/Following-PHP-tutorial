24ac9bb6cb9c79bcf81280550f7c8a163e5a77b3
zitj <1eyedea2eyedeas@gmail.com>
2020-06-08 12:37:01 +0200

    lesson about strings

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 $firstVariable = 'I am the first variable.';
 define('CONSTANT_VARIABLE', 'I am the constant variable');
+$stringOne = 'Concatination is a ';
+$stringTwo = 'cool thing.';
+$stringLength = strlen($stringOne);
 ?>
 
 <!DOCTYPE html>
@@ -13,5 +16,15 @@ define('CONSTANT_VARIABLE', 'I am the constant variable');
 <body>
     <h1><?php echo $firstVariable ?></h1>
     <h1><?php echo CONSTANT_VARIABLE ?></h1>
+    <h1><?php echo $stringOne . $stringTwo ?></h1>
+    <h1><?php echo "$stringOne $stringTwo" ?></h1>
+    <h1><?php echo "You have to quote \"like this\"." ?></h1>
+    <h1><?php echo 'Or "like this".' ?></h1>
+    <h1><?php echo "The first letter of concatanation is $stringOne[0]" ?></h1>
+    <h1><?php echo "String length of \"stringOne\" is $stringLength" ?></h1>
+    <h1><?php echo strtoupper($stringTwo) . ' function string to upper case' ?></h1>
+    <h1><?php echo strtolower($stringTwo) . ' function string to lower case' ?></h1>
+    <h1><?php echo str_replace('o', '0', $stringTwo) . ' function string replace' ?></h1>
+    
 </body>
 </html>
