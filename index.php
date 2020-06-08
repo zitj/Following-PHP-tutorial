d3f2fd3812d725cd9bfe8de1dfa44af4265c5472
zitj <1eyedea2eyedeas@gmail.com>
2020-06-08 11:39:11 +0200

    First step second time

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
 
-echo 'hello world!';
+//echo 'hello world!';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php tutorial ever</title>
+</head>
+<body>
+    <h1><?echo 'Hello world!' ?></h1>
+</body>
+</html>
