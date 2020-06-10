ce2781a92f25925e6be6333f6739e35071e3f4b1
zitj <1eyedea2eyedeas@gmail.com>
2020-06-10 12:42:06 +0200

    add materialize.css & .js

--- Ninja pizzas project/footer.php
+++ Ninja pizzas project/footer.php
@@ -1,1 +1,5 @@
+<footer class="section">
+    <div class="center grey-text">Copyright 2020 Ninja Pizzas</div>
+</footer>
+
 </body>
--- Ninja pizzas project/header.php
+++ Ninja pizzas project/header.php
@@ -1,8 +1,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+     <script type="text/javascript" src="js/materialize.min.js" defer></script>
     <title>Ninja pizzas</title>
+    <style type="text/css">
+    .brand{
+        background: #cbb09c !important;
+    }
+    .brand-text{
+        color: #cbb09c !important;
+    }
+    </style>
 </head>
-<body>
+<body class="grey lighten-4">
 
-<h1>this works</h1>
+
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
+        </div>
+
+</nav>
+
