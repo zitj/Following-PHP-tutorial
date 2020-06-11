5f8ae2a5506c49eb272faaa80bcb1465c3a3d356
zitj <1eyedea2eyedeas@gmail.com>
2020-06-11 14:05:27 +0200

    creating an array with explode() function;

--- Ninja pizzas project/index.php
+++ Ninja pizzas project/index.php
@@ -8,7 +8,7 @@
     }
     
     //write query for all pizzas
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     //make query & get result
     $result = mysqli_query($connection, $sql);
@@ -33,12 +33,16 @@
         <div class="row">
 
         <?php foreach($pizzas as $pizza) { ?>
-
+            <?php $arrayOfIngredients = explode(',', $pizza['ingredients']);?>
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6><?php echo htmlspecialchars($pizza['title'])?></h6>
-                        <div><?php echo htmlspecialchars($pizza['ingredients'])?></div>
+                        <ul>
+                            <?php foreach($arrayOfIngredients as $ingredient){?>
+                                <li><?php echo htmlspecialchars($ingredient);?></li>
+                            <?php }?>
+                        </ul>
                     </div><!-- end .card-content -->
                     <div class="card-action right-align">
                         <a class="brand-text" href="#">more info</a>
