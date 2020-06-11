c1d4f34138f6944dd0a67044482c9216dc5d3425
zitj <1eyedea2eyedeas@gmail.com>
2020-06-11 13:13:34 +0200

    writing and printing query as a result

--- Ninja pizzas project/index.php
+++ Ninja pizzas project/index.php
@@ -6,7 +6,18 @@
     if(!$connection){
         echo 'Connection error' . mysqli_connect_error();
     }
+    
+    //write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
 
+    //make query & get result
+    $result = mysqli_query($connection, $sql);
+
+    //fetch the resulting rows as an array
+
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    print_r($pizzas);
 
 ?>
 <!DOCTYPE html>
