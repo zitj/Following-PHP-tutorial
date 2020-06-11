edd0ef8f9b822273b9ffb101fa815d1a518e3d99
zitj <1eyedea2eyedeas@gmail.com>
2020-06-11 13:00:51 +0200

    connecting to MySQLi database

--- Ninja pizzas project/index.php
+++ Ninja pizzas project/index.php
@@ -1,5 +1,12 @@
 <?php 
 
+    //connect to MySQLi database
+    $connection = mysqli_connect('localhost', 'zitj', 'tjzi1389', 'ninja_pizza');
+
+    if(!$connection){
+        echo 'Connection error' . mysqli_connect_error();
+    }
+
 
 ?>
 <!DOCTYPE html>
